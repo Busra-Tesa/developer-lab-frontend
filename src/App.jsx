f67f0e3d42f6488a1c342e651f72e2ce240b8363
Tesa Musa <kujtesamusa1@gmail.com>
2024-03-08 10:48:57 +0100

    Add router dependencies,and add loaded PostPage from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import PostPage from './pages/PostPage'
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+import { Route,Routes } from 'react-router-dom'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   
+      <Routes>
        
+        <Route path="/post" element={<PostPage />} />
+      </Routes>
     </>
-  )
+     
+   
+  );
 }
 
 export default App
---
+++ src/pages/PostPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PostPage(){
+    return(
+        <>
+        <h1>This is the POST PAGE</h1>
+        </>
+    )
+}
+
+export default PostPage;
