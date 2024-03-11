20aa52385b529f4866df42315e43d1da17046b83
Tesa Musa <kujtesamusa1@gmail.com>
2024-03-11 14:56:32 +0100

    Add navigation on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -72,7 +72,7 @@ function HomePage() {
 
     <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-            <h2 className="mb-8 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 lg:mb-16 dark:text-white md:text-4xl">You’ll be in good company</h2>
+            <h2 className="mb-8 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 lg:mb-16 dark:text-white md:text-2xl">Our posts are inspired by articles from platforms like these:</h2>
             <div className="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
              
             </div>
