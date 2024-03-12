713cefbfdd890522bee955a23f9f1ebefe934c5b
Tesa Musa <kujtesamusa1@gmail.com>
2024-03-12 10:20:49 +0100

    Rename poslist

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import PostList from '../components/Postlist';
+import PostList from '../components/PostList';
 
 
 function PostPage() {
