6017498a84f6f3a79e60f88bff0c8c0b614cb900
Tesa Musa <kujtesamusa1@gmail.com>
2024-03-12 19:18:25 +0100

    Rename the imported file PostList

--- src/components/Postlist.jsx
+++ src/components/Postlist.jsx
@@ -1,5 +1,6 @@
-
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+
 
 function PostList({ posts }) {
   console.log("Posts:", posts); 
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import PostList from '../components/PostList';
+import PostList from '../components/PostList.jsx';
 
 
 function PostPage() {
